027f044119c50af4576b73c2b0def1216fc273c2
rlaehrb1 <rlaehrb11@gmail.com>
2021-07-10 23:36:12 +0900

    정보객체표현 응용2

--- src/main/kotlin/tostring.kt
+++ src/main/kotlin/tostring.kt
@@ -4,21 +4,24 @@ fun main(){
     article1.age = 30
     article1.sex = "남자"
     article1.city = "전주"
-    println("이름 : ${article1.id}, 성별 : ${article1.sex}, 나이 : ${article1.age}, 사는곳 : ${article1.city} ")
+    println(article1)
+    //println("이름 : ${article1.id}, 성별 : ${article1.sex}, 나이 : ${article1.age}, 사는곳 : ${article1.city} ")
 
     val article2 = Article1()
     article2.id = "이서이"
     article2.age = 28
     article2.sex = "여자"
     article2.city = "전주"
-    println("이름 : ${article2.id}, 성별 : ${article2.sex}, 나이 : ${article2.age}, 사는곳 : ${article2.city} ")
+    println(article2)
+    //println("이름 : ${article2.id}, 성별 : ${article2.sex}, 나이 : ${article2.age}, 사는곳 : ${article2.city} ")
 
     val article3 = Article1()
     article3.id = "홍길동"
     article3.age = 40
     article3.sex = "남자"
     article3.city = "서울"
-    println("이름 : ${article3.id}, 성별 : ${article3.sex}, 나이 : ${article3.age}, 사는곳 : ${article3.city} ")
+    println(article3)
+    //println("이름 : ${article3.id}, 성별 : ${article3.sex}, 나이 : ${article3.age}, 사는곳 : ${article3.city} ")
 
 }
 
@@ -27,6 +30,10 @@ class Article1{
     var sex: String = ""
     var age: Int = 0
     var city : String = ""
+
+    override fun toString(): String {        // << 코드를 간단하게 하기 !!
+        return "id: $id, sex: $sex, age: $age, city: $city "
+    }
 
 
 }
