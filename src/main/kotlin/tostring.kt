8516ee495ba01973ea5993bfa0bcd311e8163684
rlaehrb1 <rlaehrb11@gmail.com>
2021-07-11 00:00:26 +0900

    메서드

--- src/main/kotlin/tostring.kt
+++ src/main/kotlin/tostring.kt
@@ -4,7 +4,8 @@ fun main(){
     article1.age = 30
     article1.sex = "남자"
     article1.city = "전주"
-    println(article1)
+    println(article1.introduceMsg())
+    //println(article1)
     //println("이름 : ${article1.id}, 성별 : ${article1.sex}, 나이 : ${article1.age}, 사는곳 : ${article1.city} ")
 
     val article2 = Article1()
@@ -12,7 +13,8 @@ fun main(){
     article2.age = 28
     article2.sex = "여자"
     article2.city = "전주"
-    println(article2)
+    println(article2.introduceMsg())
+    //println(article2)
     //println("이름 : ${article2.id}, 성별 : ${article2.sex}, 나이 : ${article2.age}, 사는곳 : ${article2.city} ")
 
     val article3 = Article1()
@@ -20,7 +22,8 @@ fun main(){
     article3.age = 40
     article3.sex = "남자"
     article3.city = "서울"
-    println(article3)
+    println(article3.introduceMsg())
+    //println(article3)
     //println("이름 : ${article3.id}, 성별 : ${article3.sex}, 나이 : ${article3.age}, 사는곳 : ${article3.city} ")
 
 }
@@ -30,6 +33,10 @@ class Article1{
     var sex: String = ""
     var age: Int = 0
     var city : String = ""
+
+    fun introduceMsg(): String{
+        return "안녕하세요! 저는 $id 입니다. ${age}살이고 $city 에 살고있습니다."
+    }
 
     override fun toString(): String {        // << 코드를 간단하게 하기 !!
         return "id: $id, sex: $sex, age: $age, city: $city "
